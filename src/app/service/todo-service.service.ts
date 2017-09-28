3f0d72c8c0339f73b22538ff5fd5d4a323597835
Sitesh <siteshgjha@gmail.com>
2017-09-28 17:23:33 +0530

    Add crud todo function in todo-service

--- src/app/service/todo-service.service.ts
+++ src/app/service/todo-service.service.ts
@@ -9,20 +9,36 @@ export class TodoService {
   constructor(private http: Http) { }
 
     getUsers(): Observable<IUsers[]> {   
-  		return this.http.get("https://jsonplaceholder.typicode.com/users")
-  			.map(response => response.json())
-  			.catch(this.errorHandle);
-		}
+		return this.http.get("https://jsonplaceholder.typicode.com/users")
+			.map(response => response.json())
+			.catch(this.errorHandle);
+	}
 
-		getUser(id:number): Observable<IUser[]> {
-			return this.http.get("https://jsonplaceholder.typicode.com/todos?userId=" + id)
-				.map(response => response.json())
-				.catch(this.errorHandle);
-		}
+	getUser(id:number): Observable<IUser[]> {
+		return this.http.get("https://jsonplaceholder.typicode.com/todos?userId=" + id)
+			.map(response => response.json())
+			.catch(this.errorHandle);
+	}
 
-		setUser() {}
+	createTodo(data:IUser) {
+		return this.http.post("http://jsonplaceholder.typicode.com/posts", data)
+			.map(response => response.json())
+			.catch(this.errorHandle);
+	}
 
-		errorHandle(error:any) {
-			return Observable.throw(error.json().error || 'Server error');
-		}
+	updateTodo(data:IUser, id:number) {
+		return this.http.put("http://jsonplaceholder.typicode.com/posts/" + id, data)
+			.map(response => response.json())
+			.catch(this.errorHandle);
+	}
+
+	deleteTodo(id:number) {
+		return this.http.delete("http://jsonplaceholder.typicode.com/posts/" + id)
+			.map(response => response.json())
+			.catch(this.errorHandle);
+	}
+
+	errorHandle(error:any) {
+		return Observable.throw(error.json().error || 'Server error');
+	}
 }
