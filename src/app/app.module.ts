2a217acc960b1f52d0693e483a0a76829806cd86
Sitesh <siteshgjha@gmail.com>
2017-09-28 11:30:37 +0530

    Initialize component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+import { TodoService } from './service/todo-service.service';
+import { HttpModule } from '@angular/http';
+import { ClarityModule } from 'clarity-angular';
+import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
+import { routing } from './routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    NavComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    ClarityModule.forRoot(),
+    routing
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
